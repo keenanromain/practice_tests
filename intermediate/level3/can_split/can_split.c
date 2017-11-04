b5ea9fbbf40a0dbd9287f3dab456a2a091596c4b
Keenan ROMAIN <kromain@e1z2r7p26.42.us.org>
2017-11-03 17:45:34 -0700

    naive vs better solution for can_split

--- intermediate/level3/can_split/can_split.c
+++ intermediate/level3/can_split/can_split.c
@@ -10,23 +10,31 @@ int		tree_size(struct s_node *n)
 	return (n ? 1 + tree_size(n->left) + tree_size(n->right) : 0);
 }
 
-int		check_nodes(struct s_node *n, int total)
+int		check_nodes(struct s_node *n, int total, int *ret)
 {
 	if (n)
 	{
-		if (tree_size(n) == total - tree_size(n))
-			return (1);
-		return (check_nodes(n->left, total) || check_nodes(n->right, total));
+		int curr;
+
+		curr = 1 + check_nodes(n->left, total, ret) + check_nodes(n->right, total, ret);
+		if (curr == total - curr)
+			*ret = 1;
 	}
 	return (0);
 }
 
 int		can_split(struct s_node *n)
 {
+	int ret;
+
+	ret = 0;
 	if (n)
 	{
-		int total = tree_size(n);
-		return (check_nodes(n, total));
+		int total;
+
+		total = tree_size(n);
+		check_nodes(n, total, &ret);
 	}
-	return (0);
+	return (ret);
 }
+
---
+++ intermediate/level3/can_split/naive.c
@@ -0,0 +1,34 @@
+struct s_node
+{
+	int value;
+	struct s_node *left;
+	struct s_node *right;
+};
+
+int		tree_size(struct s_node *n)
+{
+	return (n ? 1 + tree_size(n->left) + tree_size(n->right) : 0);
+}
+
+int		check_nodes(struct s_node *n, int total)
+{
+	if (n)
+	{
+		int root = tree_size(n);
+
+		if (root == total - root)
+			return (1);
+		return (check_nodes(n->left, total) || check_nodes(n->right, total));
+	}
+	return (0);
+}
+
+int		can_split(struct s_node *n)
+{
+	if (n)
+	{
+		int total = tree_size(n);
+		return (check_nodes(n, total));
+	}
+	return (0);
+}
